5db429f63d9722ac408216cfa9431deabfbde600
Your Name <you@example.com>
2024-12-31 17:27:25 +0500

    logic bulding

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-let useer_input = document.getElementById("");
-useer_input.add
+let useer_input = document.getElementById("prompt");
+useer_input.addEventListener("keydown" , (e) => {
+    if (e.key === "Enter"){
+        console.log(useer_input.value);
+        
+    }
+    
+} )
